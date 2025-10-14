1fe33e8a4b41f8bf8dbebfc7e98045e482c079b2
maxrchung <maxrchung@gmail.com>
2025-10-13 21:14:31 -0700

    some menu focus stuff i'm gonna revert

--- apps/client/src/components/SettingsButton.vue
+++ apps/client/src/components/SettingsButton.vue
@@ -7,9 +7,77 @@ import { useStore } from '../store';
 
 const isOpen = ref(false);
 const store = useStore();
+const buttonRef = ref<InstanceType<typeof TooltipButton> | null>(null);
+const itemsRef = ref<HTMLButtonElement[]>([]);
+
+const setItemsRef = (button: HTMLButtonElement) => {
+  debugger;
+  if (!button) return;
+  itemsRef.value.push(button);
+};
 
 const openSettings = () => {
   isOpen.value = !isOpen.value;
+
+  if (isOpen.value) {
+    const firstItem = itemsRef.value[0];
+    if (firstItem) {
+      firstItem.focus();
+    }
+  } else {
+    buttonRef.value?.buttonRef.focus();
+  }
+};
+
+const handleKeyDown = (e: KeyboardEvent) => {
+  if (!isOpen.value) return;
+
+  const items = itemsRef.value;
+  const activeIndex = items.indexOf(
+    document.activeElement as HTMLButtonElement,
+  );
+
+  switch (e.key) {
+    case 'ArrowDown':
+    case 'ArrowRight':
+      e.preventDefault();
+      if (activeIndex === -1) {
+        items[0].focus();
+        break;
+      }
+
+      const nextIndex = (activeIndex + 1) % items.length;
+      items[nextIndex].focus();
+      break;
+
+    case 'ArrowUp':
+    case 'ArrowLeft':
+      e.preventDefault();
+      if (activeIndex === -1) {
+        items[items.length - 1].focus();
+        break;
+      }
+
+      const prevIndex = (activeIndex - 1) % items.length;
+      items[prevIndex].focus();
+      break;
+
+    case 'Home':
+      e.preventDefault();
+      items[0].focus();
+      break;
+
+    case 'End':
+      e.preventDefault();
+      items[items.length - 1].focus();
+      break;
+
+    case 'Escape':
+      e.preventDefault();
+      isOpen.value = false;
+      buttonRef.value?.buttonRef.focus();
+      break;
+  }
 };
 
 const resetGame = () => {
@@ -19,7 +87,7 @@ const resetGame = () => {
 </script>
 
 <template>
-  <div class="relative" @mouseleave="isOpen = false">
+  <div class="relative" @mouseleave="isOpen = false" @keydown="handleKeyDown">
     <TooltipButton
       tooltip="Open settings..."
       :isOpen="isOpen"
@@ -30,6 +98,7 @@ const resetGame = () => {
       :class="{
         'border-b-transparent': isOpen,
       }"
+      ref="buttonRef"
     >
       <WrenchScrewdriverIcon class="size-6" />
     </TooltipButton>
@@ -56,6 +125,7 @@ const resetGame = () => {
           <button
             class="flex cursor-pointer gap-2 border-b-1 border-l-1 bg-stone-100 px-3 py-2 transition hover:bg-stone-200 dark:bg-stone-900 dark:hover:bg-stone-800"
             @click="resetGame"
+            ref="setItemsRef"
           >
             <ArrowPathIcon class="size-6" />
             Reset game
