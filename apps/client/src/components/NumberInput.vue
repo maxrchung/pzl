d20377f51dc1b84266c93861bcbb8e65e057daee
maxrchung <maxrchung@gmail.com>
2025-11-17 21:32:29 -0800

    inputmode numeric

--- apps/client/src/components/NumberInput.vue
+++ apps/client/src/components/NumberInput.vue
@@ -1,6 +1,7 @@
 <template>
   <input
     type="number"
+    inputmode="numeric"
     class="bg-stone border px-3 py-1 hover:bg-stone-200 dark:bg-stone-900 dark:hover:bg-stone-800"
     v-bind="$attrs"
   />
