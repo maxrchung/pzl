02316dbdeb5b2b5e4ad384eee75d9a5944289965
maxrchung <maxrchung@gmail.com>
2026-02-20 00:50:29 -0800

    key

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +19,20 @@ export class CdkStack extends cdk.Stack {
       allowedMethods: [s3.HttpMethods.POST],
       allowedHeaders: ['*'],
       maxAge: 3000,
+    });
+
+    const user = new iam.User(this, 'pzl-user', { userName: 'pzl-user' });
+
+    bucket.grantReadWrite(user);
+
+    const key = new iam.AccessKey(this, 'pzl-access-key', { user });
+    new ssm.StringParameter(this, 'pzl-key', {
+      parameterName: 'pzl-key',
+      stringValue: key.accessKeyId,
+    });
+    new ssm.StringParameter(this, 'pzl-secret', {
+      parameterName: 'pzl-secret',
+      stringValue: key.secretAccessKey.unsafeUnwrap(),
     });
   }
 }
